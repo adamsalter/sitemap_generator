101c8ddff06c804e4c16b9d15969785eb9d76da3
Adam Salter <adam@codebright.net>
2009-07-02 12:30:07 +1000

    - update config/sitemap.rb

--- templates/sitemap.rb
+++ templates/sitemap.rb
@@ -1,20 +1,25 @@
-
 # Set the host name for URL creation
 SitemapPlugin::Sitemap.default_host = "http://www.example.com"
 
-# Put links creation logic here (the root path '/' and sitemap files are added automatically)
-# Usage:
-# sitemap.add path, options = { :priority => 0.5, :changefreq => 'weekly', :lastmod => Time.now, :host => default_host }
-# default options are used if you don't specify
+# Put links creation logic here.
+#
+# The root path '/' and sitemap index file are added automatically.
+# Links are added to the Sitemap in the order they are specified.
+#
+# Usage: sitemap.add path, options
+#        (default options are used if you don't specify)
+#
+# Defaults: :priority => 0.5, :changefreq => 'weekly', 
+#           :lastmod => Time.now, :host => default_host
 SitemapPlugin::Sitemap.add_links do |sitemap|
   # add '/articles'
-  sitemap.add articles_path # :priority => 0.5, :changefreq => 'weekly', :lastmod => Time.now, :host => default_host
+  sitemap.add articles_path, :priority => 0.7, :changefreq => 'daily'
 
   # add all individual articles
   Article.find(:all).each do |a|
     sitemap.add article_path(a), :lastmod => a.updated_at
   end
-  
+
   # add merchant path
-  sitemap.add '/purchase', :host => "https://www.example.com"
+  sitemap.add '/purchase', :priority => 0.7, :host => "https://www.example.com"
 end
