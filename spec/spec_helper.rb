8cf663ad95a0ce7ae81aa1d32dbf7adcd1932b2a
Adam Salter <adam@codebright.net>
2009-09-14 17:44:07 +1000

    - move to autotest

--- spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-ENV['RAILS_ENV'] = 'test'
-ENV['RAILS_ROOT'] ||= File.join(File.dirname(__FILE__), 'mock_app')
-
-require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config', 'environment.rb'))
-
-require 'spec'
-require 'spec/rails'
