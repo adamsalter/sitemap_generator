b7e1555de1954ebae3c80073940d3c9b79739a48
Karl Varga <kjvarga@gmail.com>
2009-12-11 21:30:54 +0800

    Test as a Gem install rather than a plugin.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,12 @@
 ENV['RAILS_ENV'] = 'test'
-ENV['RAILS_ROOT'] ||= File.join(File.dirname(__FILE__), 'mock_app')
 
-require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config', 'environment.rb'))
-
-require 'fileutils'
+# This is basically the contents of mock_app_gems's Rakefile
+require File.join(File.dirname(__FILE__), 'mock_app_gem', 'config', 'boot')
 require 'rake'
-require 'shoulda'
-
 require 'rake/testtask'
 require 'rake/rdoctask'
 require 'tasks/rails'
+require 'sitemap_generator/tasks'
+
+# Testing
+require 'shoulda'
