7c90742b5bb2d4ad9e52b39409de6447c699ffbf
Adrian Mugnolo <adrian@mugnolo.com>
2010-03-17 22:16:29 -0300

    Clean up bootstrap code

--- lib/sitemap_generator.rb
+++ lib/sitemap_generator.rb
@@ -4,16 +4,18 @@ require 'sitemap_generator/link_set'
 require 'sitemap_generator/helper'
 
 module SitemapGenerator
-  class <<self
+  VERSION = File.read(File.dirname(__FILE__) + "/../VERSION").strip
+
+  class << self
     attr_accessor :root, :templates
   end
+
   self.root = File.expand_path(File.join(File.dirname(__FILE__), '../'))
   self.templates = {
-    :sitemap_index => File.join(self.root, 'templates/sitemap_index.builder'),
-    :sitemap_xml   => File.join(self.root, 'templates/xml_sitemap.builder'),
+    :sitemap_index  => File.join(self.root, 'templates/sitemap_index.builder'),
+    :sitemap_xml    => File.join(self.root, 'templates/xml_sitemap.builder'),
     :sitemap_sample => File.join(self.root, 'templates/sitemap.rb'),
   }
-  
+
   Sitemap = LinkSet.new
 end
-  
