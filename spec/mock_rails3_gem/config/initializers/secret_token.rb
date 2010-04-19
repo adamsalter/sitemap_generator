2efb7dfd4d3cc5779e5c358773bb0e813fb81cda
Karl Varga <kjvarga@gmail.com>
2010-04-19 13:15:04 -0700

    Add mock Rails 3 app

---
+++ spec/mock_rails3_gem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '0be394414161860f4e3f68b42b4b19ddbbbd69261bd930bd67912097061264cc478c77b4b77d0e8f1c4e0f094c7783c037bef2882cd5960a02b6c158c3637554'
