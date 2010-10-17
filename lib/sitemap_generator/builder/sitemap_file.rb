adamsalter/sitemap_generator

commit ab96c6d38dcfd1fb27639aedee4b0f17a1857a3d

Refactor to get rid of Link and Mapper

lib/sitemap_generator/builder/sitemap_file.rb
@@ -7,10 +7,9 @@ module SitemapGenerator
     # General Usage:
     #
     #   sitemap = SitemapFile.new('public/', 'sitemap.xml', 'http://example.com')
-    #       <- creates a new sitemap file in directory public/
-    #   sitemap.add_link({ ... })    <- add a link to the sitemap
-    #   sitemap.finalize!            <- write and close the sitemap file and protect it
-    #       from further modification
+    #   sitemap.add('/', { ... })    <- add a link to the sitemap
+    #   sitemap.finalize!            <- creates a new sitemap file in directory public/
+    #       and freezes the object to protect it from further modification
     #
     class SitemapFile
       attr_accessor :sitemap_path, :public_path, :filesize, :link_count, :hostname
@@ -80,7 +79,7 @@ module SitemapGenerator
       #   sitemap_url - a SitemapUrl instance
       #   sitemap, options - a Sitemap instance and options hash
       #   path, options - a path for the URL and options hash
-      def add_link(link, options={})
+      def add(link, options={})
         xml = if link.is_a?(SitemapGenerator::Builder::SitemapUrl)
           link.to_xml
         else
@@ -99,7 +98,7 @@ module SitemapGenerator
         self.link_count += 1
         true
       end
-      alias_method :<<, :add_link
+      alias_method :<<, :add
 
       # Write out the Sitemap file and freeze this object.
       #

lib/sitemap_generator.rb
@@ -1,6 +1,4 @@
 require 'sitemap_generator/builder'
-require 'sitemap_generator/mapper'
-require 'sitemap_generator/link'
 require 'sitemap_generator/link_set'
 require 'sitemap_generator/templates'
 require 'sitemap_generator/utilities'
@@ -9,8 +7,9 @@ require 'sitemap_generator/railtie' if SitemapGenerator::Utilities.rails3?
 require 'active_support/core_ext/numeric'
 
 module SitemapGenerator
-  class SitemapFull < StandardError; end
-  class SitemapFinalized < StandardError; end
+  class SitemapError <StandardError; end
+  class SitemapFullError < SitemapError; end
+  class SitemapFinalizedError < SitemapError; end
   
   silence_warnings do
     VERSION = File.read(File.dirname(__FILE__) + "/../VERSION").strip

lib/sitemap_generator/builder.rb
@@ -1,6 +1,7 @@
-require 'sitemap_generator/builder/helper'
 require 'sitemap_generator/builder/sitemap_file'
 require 'sitemap_generator/builder/sitemap_index_file'
+require 'sitemap_generator/builder/sitemap_url'
+require 'sitemap_generator/builder/sitemap_index_url'
 
 module SitemapGenerator
   module Builder

lib/sitemap_generator/builder/helper.rb
@@ -1,10 +1,0 @@
-module SitemapGenerator
-  module Builder
-    module Helper
-
-      def w3c_date(date)
-         date.utc.strftime("%Y-%m-%dT%H:%M:%S+00:00")
-      end
-    end
-  end
-end

lib/sitemap_generator/builder/sitemap_index_file.rb
@@ -1,12 +1,15 @@
+require 'action_view'
+
 module SitemapGenerator
   module Builder
     class SitemapIndexFile < SitemapFile
-
+      include ActionView::Helpers::NumberHelper  # for number_with_delimiter
       attr_accessor :sitemaps
 
       def initialize(*args)
         super(*args)
 
+        self.sitemaps = []
         @xml_content = '' # XML urlset content
         @xml_wrapper_start = <<-HTML
           <?xml version="1.0" encoding="UTF-8"?>
@@ -22,14 +25,15 @@ module SitemapGenerator
         self.filesize = bytesize(@xml_wrapper_start) + bytesize(@xml_wrapper_end)
       end
 
-      # Output a summary line
+      # Return a summary line
       def summary(start_time=nil, end_time=nil)
-        puts "\nSitemap stats: #{number_with_delimiter(self.link_count)} links / #{self.sitemaps.size} files / "
-        put ("%dm%02ds" % (end_time - start_time).divmod(60)) if start_time && end_time
+        str = "\nSitemap stats: #{number_with_delimiter(self.link_count)} links / #{self.sitemaps.size} files / "
+        str += ("%dm%02ds" % (end_time - start_time).divmod(60)) if start_time && end_time
+        str
       end
 
       # Finalize sitemaps as they are added to the index
-      def add_link(link, options={})
+      def add(link, options={})
         if link.is_a?(SitemapFile)
           self.sitemaps << link
           link.finalize!

lib/sitemap_generator/builder/sitemap_url.rb
@@ -87,6 +87,10 @@ module SitemapGenerator
         end
         images[0..(SitemapGenerator::MAX_SITEMAP_IMAGES-1)]
       end
+
+      def w3c_date(date)
+         date.utc.strftime("%Y-%m-%dT%H:%M:%S+00:00")
+      end
     end
   end
 end

lib/sitemap_generator/link.rb
@@ -1,37 +1,0 @@
-module SitemapGenerator
-  module Link
-    extend self
-
-    # Return a Hash of options suitable to pass to SitemapGenerator::Builder::SitemapFile#add_link.
-    def generate(path, options = {})
-      if path.is_a?(SitemapGenerator::Builder::SitemapFile)
-        options.reverse_merge!(:host => path.hostname, :lastmod => path.lastmod)
-        path = path.sitemap_path
-      end
-
-      options.assert_valid_keys(:priority, :changefreq, :lastmod, :host, :images, :video)
-      options.reverse_merge!(:priority => 0.5, :changefreq => 'weekly', :lastmod => Time.now, :host => Sitemap.default_host, :images => [])
-      {
-        :path => path,
-        :priority => options[:priority],
-        :changefreq => options[:changefreq],
-        :lastmod => options[:lastmod],
-        :host => options[:host],
-        :loc => URI.join(options[:host], path).to_s,
-        :images => prepare_images(options[:images], options[:host]),
-        :video => options[:video]
-      }
-    end
-
-    # Return an Array of image option Hashes suitable to be parsed by SitemapGenerator::Builder::SitemapFile
-    def prepare_images(images, host)
-      images.delete_if { |key,value| key[:loc] == nil }
-      images.each do |r|
-        r.assert_valid_keys(:loc, :caption, :geo_location, :title, :license)
-        r[:loc] = URI.join(host, r[:loc]).to_s
-      end
-      images[0..(SitemapGenerator::MAX_SITEMAP_IMAGES-1)]
-    end
-  end
-end
-

lib/sitemap_generator/link_set.rb
@@ -8,7 +8,7 @@ module SitemapGenerator
     include ActionView::Helpers::NumberHelper  # for number_with_delimiter
 
     attr_accessor :default_host, :public_path, :sitemaps_path
-    attr_accessor :sitemap, :sitemaps, :sitemap_index
+    attr_accessor :sitemap, :sitemap_index
     attr_accessor :verbose, :yahoo_app_id
 
     # Evaluate the sitemap config file and write all sitemaps.
@@ -50,11 +50,10 @@ module SitemapGenerator
       self.default_host = default_host
       self.public_path = public_path
       self.sitemaps_path = sitemaps_path
-      self.sitemaps = [] # Completed sitemaps
     end
 
     def link_count
-      self.sitemaps.inject(0) { |link_count_sum, sitemap| link_count_sum + sitemap.link_count }
+      self.sitemap_index.sitemaps.inject(0) { |link_count_sum, sitemap| link_count_sum + sitemap.link_count }
     end
 
     # Entry point for users.
@@ -68,27 +67,24 @@ module SitemapGenerator
 
       # Set default host on the sitemap objects and seed the sitemap with the default links
       self.sitemap.hostname = self.sitemap_index.hostname = default_host
-      self.sitemap.add_link('/', :lastmod => Time.now, :changefreq => 'always', :priority => 1.0)
-      self.sitemap.add_link(self.sitemap_index, :lastmod => Time.now, :changefreq => 'always', :priority => 1.0)
+      self.sitemap.add('/', :lastmod => Time.now, :changefreq => 'always', :priority => 1.0)
+      self.sitemap.add(self.sitemap_index, :lastmod => Time.now, :changefreq => 'always', :priority => 1.0)
 
-      yield Mapper.new(self)
+      yield self
     end
 
     # Add a link to a Sitemap.  If a new Sitemap is required, one will be created for
     # you.
-    #
-    # Called from Mapper.
-    def add_link(link)
+    def add(link, options={})
       begin
-        self.sitemap << link
-      rescue SitemapGenerator::SitemapFull
-        self.sitemap_index << self.sitemap
-        puts self.sitemap.summary if verbose
+        self.sitemap.add(link, options)
+      rescue SitemapGenerator::SitemapError => e
+        if e.is_a?(SitemapGenerator::SitemapFullError)
+          self.sitemap_index << self.sitemap
+          puts self.sitemap.summary if verbose
+        end
         self.sitemap = SitemapGenerator::Builder::SitemapFile.new(public_path, new_sitemap_path, default_host)
-        self.sitemap << link
-      rescue SitemapGenerator::SitemapFinalized
-        self.sitemap = SitemapGenerator::Builder::SitemapFile.new(public_path, new_sitemap_path, default_host)
-        self.sitemap << link
+        self.sitemap.add(link, options)
       end
     end
 
@@ -136,7 +132,7 @@ module SitemapGenerator
     #
     # The index depends on the length of the <tt>sitemaps</tt> array.
     def new_sitemap_path
-      File.join(self.sitemaps_path || '', "sitemap#{self.sitemaps.length + 1}.xml.gz")
+      File.join(self.sitemaps_path || '', "sitemap#{self.sitemap_index.sitemaps.length + 1}.xml.gz")
     end
 
     # Return the current sitemap index filename.

commit 464478cf1f3d80494d26fd40ced916b336b2210b

Fix "uninitialized constant" error when sitemap file is full.
Error was introduced in refactoring by ab96c6d38dcfd1fb27639aedee4b0f17a1857a3d.

lib/sitemap_generator/builder/sitemap_file.rb
@@ -72,7 +72,7 @@ module SitemapGenerator
       # Add a link to the sitemap file.
       #
       # If a link cannot be added, for example if the file is too large or the link
-      # limit has been reached, a SitemapGenerator::SitemapFull exception is raised.
+      # limit has been reached, a SitemapGenerator::SitemapFullError exception is raised.
       #
       # If the Sitemap has already been finalized a SitemapGenerator::SitemapFinalized
       # exception is raised.
@@ -91,7 +91,7 @@ module SitemapGenerator
         if self.finalized?
           raise SitemapGenerator::SitemapFinalized
         elsif !file_can_fit?(bytesize(xml))
-          raise SitemapGenerator::SitemapFull
+          raise SitemapGenerator::SitemapFullError
         end
 
         # Add the XML