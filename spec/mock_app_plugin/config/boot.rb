0532c06ec5cccac7604f00a6c2676b72cf6cc9fd
Karl Varga <kjvarga@gmail.com>
2010-12-06 15:10:23 -0800

    Configure the plugin version to use Bundler and dynamically load schema

--- spec/mock_app_plugin/config/boot.rb
+++ spec/mock_app_plugin/config/boot.rb
@@ -5,6 +5,20 @@ RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
 
 module Rails
   class << self
+    class Rails::Boot
+      def run
+        load_initializer
+
+        Rails::Initializer.class_eval do
+          def load_gems
+            @bundler_loaded ||= Bundler.require :default, Rails.env
+          end
+        end
+
+        Rails::Initializer.run(:set_load_path)
+      end
+    end
+    
     def boot!
       unless booted?
         preinitialize
