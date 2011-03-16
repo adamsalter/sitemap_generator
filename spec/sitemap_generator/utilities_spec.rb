e206e18303c7493a5615fa14db859c1153ffb86a
Karl Varga <kjvarga@gmail.com>
2011-03-15 23:32:42 -0700

    Add specs for Utilities.rails3?

--- spec/sitemap_generator/utilities_spec.rb
+++ spec/sitemap_generator/utilities_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe SitemapGenerator::Utilities do
 
+  context "rails3?" do
+    tests = {
+      :nil => false,
+      '2.3.11' => false,
+      '3.0.1' => true,
+      '3.0.11' => true
+    }
+    it "should identify the rails version correctly" do
+      tests.each do |version, result|
+        Rails.expects(:version).returns(version)
+        SitemapGenerator::Utilities.rails3?.should == result
+      end
+    end
+  end
 end
