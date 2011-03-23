a55374455664d352020a813f80e009946419f56f
Karl Varga <kjvarga@gmail.com>
2011-03-23 00:49:38 -0700

    wip update LinkSet specs...some failing

--- spec/sitemap_generator/link_set_spec.rb
+++ spec/sitemap_generator/link_set_spec.rb
@@ -1,26 +1,44 @@
 require 'spec_helper'
 
 describe SitemapGenerator::LinkSet do
-  options = {
-    :filename => :xxx,
-    :sitemaps_path => 'mobile/',
-    :public_path => 'tmp/',
-    :default_host => 'http://myhost.com'
-  }
 
-  it "should accept options as a hash" do
-    linkset = SitemapGenerator::LinkSet.new(options)
-    options.each do |option, value|
-      linkset.send(option).should == value
+  context "initializer options" do
+    options = [:public_path, :sitemaps_path, :default_host, :filename]
+    values = ['tmp/', 'mobile/', 'http://myhost.com', :xxx]
+
+    options.zip(values).each do |option, value|
+      it "should set #{option} to #{value}" do
+        @ls = SitemapGenerator::LinkSet.new(option => value)
+        @ls.send(option).should == value
+      end
+    end
+
+    it "should support calling with positional arguments (deprecated)" do
+      @ls = SitemapGenerator::LinkSet.new(*values[0..3])
+      options.each do |option|
+        @ls.send(option).should == value
+      end
     end
   end
 
-  it "should still support calling with positional arguments" do
-    args = [:public_path, :sitemaps_path, :default_host, :filename]
-    args = args.map { |arg| options[arg] }
-    linkset = SitemapGenerator::LinkSet.new(*args)
-    options.each do |option, value|
-      linkset.send(option).should == value
+  context "default options" do
+    default_options = {
+      :filename => :sitemap,
+      :sitemaps_path => nil,
+      :public_path => 'public/',
+      :default_host => nil,
+      :include_index => true,
+      :include_root => true
+    }
+
+    before :all do
+      @ls = SitemapGenerator::LinkSet.new
+    end
+
+    default_options.each do |option, value|
+      it "#{option} should default to #{value}" do
+        @ls.send(option).should == value
+      end
     end
   end
 
@@ -47,6 +65,47 @@ describe SitemapGenerator::LinkSet do
       @ls.include_index.should be_false
       @ls.add_links { |sitemap| }
       @ls.sitemap.link_count.should == 0
+    end
+  end
+
+  context "sitemaps_directory" do
+    before do
+      @ls = SitemapGenerator::LinkSet.new
+    end
+
+    it "should default to public/" do
+      @ls.sitemaps_directory.should == 'public/'
+    end
+
+    it "should change when the public_path is changed" do
+      @ls.public_path = 'tmp/'
+      @ls.sitemaps_directory.should == 'tmp/'
+    end
+
+    it "should change when the sitemaps_path is changed" do
+      @ls.sitemaps_path = 'sitemaps/'
+      @ls.sitemaps_directory.should == 'public/sitemaps/'
+    end
+  end
+
+  context "sitemaps_url" do
+    before do
+      @ls = SitemapGenerator::LinkSet.new
+    end
+
+    it "should not have a default" do
+      @ls.sitemaps_url.should be_nil
+    end
+
+    it "should change when the default_host is changed" do
+      @ls.default_host = 'http://one.com'
+      @ls.sitemaps_url.should == 'http://one.com'
+    end
+
+    it "should change when the sitemaps_path is changed" do
+      @ls.default_host = 'http://one.com'
+      @ls.sitemaps_path = 'sitemaps/'
+      @ls.sitemaps_url.should == 'http://one.com/sitemaps/'
     end
   end
 end
