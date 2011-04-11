d9f39d31c9677b17d85696755e9c7d5b919987a7
Karl Varga <kjvarga@gmail.com>
2011-04-11 00:26:05 -0700

    Clean up the interpreter

--- lib/sitemap_generator/interpreter.rb
+++ lib/sitemap_generator/interpreter.rb
@@ -2,8 +2,8 @@ require 'sitemap_generator'
 
 module SitemapGenerator
 
-  # Evaluate a sitemap config file within the context of a class that includes the
-  # Rails URL helpers.
+  # Provide a class for evaluating blocks, making the URL helpers from the framework
+  # and API methods available to it.
   class Interpreter
 
     if SitemapGenerator.app.rails3?
@@ -16,29 +16,40 @@ module SitemapGenerator
     # Call with a block to evaluate a dynamic config.  The only method exposed for you is
     # `add` to add a link to the sitemap object attached to this interpreter.
     #
-    # @param sitemap a sitemap object
-    # @param sitemap_config_file full path to the config file (default is config/sitemap.rb)
-    def initialize(sitemap, sitemap_config_file=nil, &block)
-      @sitemap = sitemap
-      if block_given?
-        instance_eval(&block)
-      else
-        sitemap_config_file ||= SitemapGenerator.app.root + 'config/sitemap.rb'
-        eval(File.read(sitemap_config_file), nil, sitemap_config_file.to_s)
-      end
+    # Options:
+    #   link_set - a LinkSet instance to use.  Default is SitemapGenerator::Sitemap
+    def initialize(opts={}, &block)
+      opts.reverse_merge!(:link_set => SitemapGenerator::Sitemap)
+      @linkset = opts[:link_set]
+      eval(&block) if block_given?
     end
 
     def add(*args)
-      @sitemap.add(*args)
+      @linkset.add(*args)
     end
-    
+
     def group(*args)
-      @sitemap.group(*args)
+      @linkset.group(*args)
     end
-    
-    # Evaluate the sitemap config file using the default sitemap.
-    def self.run(*args, &block)
-      new(SitemapGenerator::Sitemap, *args, &block)
+
+    # Evaluate the block in the interpreter.  Pass :yield_sitemap => true to
+    # yield the Interpreter instance to the block...for old-style calling.
+    def eval(opts={}, &block)
+      if block_given?
+        if opts[:yield_sitemap]
+          yield self
+        else
+          instance_eval(&block)
+        end
+      end
+    end
+
+    # Pass the :config_file option to evaluate a specific config file.
+    # Options:
+    #   :config_file - full path to the config file (default is config/sitemap.rb in your root directory)
+    def self.run(config_file=nil, &block)
+      config_file ||= SitemapGenerator.app.root + 'config/sitemap.rb'
+      eval(File.read(config_file), nil, config_file.to_s)
     end
   end
 end
