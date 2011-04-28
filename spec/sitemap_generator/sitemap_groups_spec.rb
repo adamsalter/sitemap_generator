a98ebcb9fbc9d4350630ec2d9a231ce4d15ca8f3
Karl Varga <kjvarga@gmail.com>
2011-04-28 01:08:41 -0700

    Working groups spec.

--- spec/sitemap_generator/sitemap_groups_spec.rb
+++ spec/sitemap_generator/sitemap_groups_spec.rb
@@ -3,24 +3,23 @@ require "spec_helper"
 describe "Sitemap Groups" do
   before :each do
     @sm = ::SitemapGenerator::LinkSet.new(:default_host => 'http://test.com')
-  end                                                                       
-  
+  end
+
   describe "sitemap filename" do
     before :each do
       FileUtils.rm_rf(SitemapGenerator.app.root + 'public/')
     end
 
-    # it "should be changed" do
-    #   @sm.create do    
-    #     group(:filename => :sitemap_en) do
-    #       debugger 
-    #       add '/en'
-    #     end
-    #   end
-    # 
-    #   file_should_exist(SitemapGenerator.app.root + 'public/sitemap_index.xml.gz')
-    #   file_should_exist(SitemapGenerator.app.root + 'public/sitemap_en1.xml.gz')
-    #   file_should_exist(SitemapGenerator.app.root + 'public/sitemap1.xml.gz')
-    # end        
+    it "should generate files" do
+      @sm.create do
+        group(:filename => :sitemap_en) do
+          add '/en'
+        end
+      end
+
+      file_should_exist(SitemapGenerator.app.root + 'public/sitemap_index.xml.gz')
+      file_should_exist(SitemapGenerator.app.root + 'public/sitemap_en1.xml.gz')
+      file_should_not_exist(SitemapGenerator.app.root + 'public/sitemap1.xml.gz')
+    end
   end
 end
