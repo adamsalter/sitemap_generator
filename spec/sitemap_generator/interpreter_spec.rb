43be597f0b625a8df161054889e32dae112ddefc
Karl Varga <kjvarga@gmail.com>
2011-05-15 23:57:35 -0700

    Use the reset! method in the interpreter spec

--- spec/sitemap_generator/interpreter_spec.rb
+++ spec/sitemap_generator/interpreter_spec.rb
@@ -5,7 +5,7 @@ describe SitemapGenerator::Interpreter do
   # The interpreter doesn't have the URL helpers included for some reason, so it
   # fails when adding links.  That messes up later specs unless we reset the sitemap object.
   after :all do
-    SitemapGenerator::Sitemap = SitemapGenerator::LinkSet.new
+    SitemapGenerator::Sitemap.reset!
   end
 
   it "should find the config file if Rails.root doesn't end in a slash" do
@@ -13,7 +13,7 @@ describe SitemapGenerator::Interpreter do
     Rails.expects(:root).returns(rails_root).at_least_once
     lambda { SitemapGenerator::Interpreter.run }.should_not raise_exception(Errno::ENOENT)
   end
-  
+
   it "should set the verbose option" do
     SitemapGenerator::Interpreter.any_instance.expects(:instance_eval)
     interpreter = SitemapGenerator::Interpreter.run(:verbose => true)
