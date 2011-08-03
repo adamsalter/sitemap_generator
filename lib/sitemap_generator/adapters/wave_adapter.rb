83ccb7d95c75407105ca6a194391e9e50479514d
Karl Varga <kjvarga@gmail.com>
2011-08-03 11:33:04 -0700

    Fix uploading to the (bucket) root on a remote server

--- lib/sitemap_generator/adapters/wave_adapter.rb
+++ lib/sitemap_generator/adapters/wave_adapter.rb
@@ -3,10 +3,14 @@ require 'carrierwave'
 module SitemapGenerator
   class WaveAdapter < ::CarrierWave::Uploader::Base
     attr_accessor :store_dir
-    
+
+    # Call with a SitemapLocation and string data
     def write(location, raw_data)
       SitemapGenerator::FileAdapter.new.write(location, raw_data)
-      self.store_dir = File.dirname(location.path_in_public)
+      directory = File.dirname(location.path_in_public)
+      if directory != '.'
+        self.store_dir = directory
+      end
       store!(open(location.path, 'rb'))
     end
   end
