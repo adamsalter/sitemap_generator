e24e1fb5e4bbd2dababd42907049ee50981022be
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-08-24 20:18:24 -0300

    Added news xmlns

--- lib/sitemap_generator/builder/sitemap_file.rb
+++ lib/sitemap_generator/builder/sitemap_file.rb
@@ -34,6 +34,7 @@ module SitemapGenerator
               xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
               xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"
               xmlns:geo="http://www.google.com/geo/schemas/sitemap/1.0"
+              xmlns:news="http://www.google.com/schemas/sitemap-news/0.9/"
             >
         HTML
         @xml_wrapper_start.gsub!(/\s+/, ' ').gsub!(/ *> */, '>').strip!
