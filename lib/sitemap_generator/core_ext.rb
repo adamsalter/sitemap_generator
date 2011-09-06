192e6de77907973273cf30b6874a237326ca7ec8
Karl Varga <kjvarga@gmail.com>
2011-09-06 14:52:21 -0700

    Fix core extension requires * Include module for Float rounding so we can use super

--- lib/sitemap_generator/core_ext.rb
+++ lib/sitemap_generator/core_ext.rb
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 Dir["#{File.dirname(__FILE__)}/core_ext/**/*.rb"].sort.each do |path|
   require path
 end
-
-=======
-Dir["#{File.dirname(__FILE__)}/core_ext/*.rb"].sort.each do |path|
-  require "sitemap_generator/core_ext/#{File.basename(path, '.rb')}"
-end
->>>>>>> Remove Rails dependencies.  Move them in-app:
