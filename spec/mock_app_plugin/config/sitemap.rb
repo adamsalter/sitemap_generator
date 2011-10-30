386551f3221e87614e0fe480d64e312c083c146f
Karl Varga <kjvarga@gmail.com>
2011-10-29 19:16:51 -0700

    Update the app sitemap config

--- spec/mock_app_plugin/config/sitemap.rb
+++ spec/mock_app_plugin/config/sitemap.rb
@@ -1,13 +1,37 @@
 SitemapGenerator::Sitemap.default_host = "http://www.example.com"
-SitemapGenerator::Sitemap.yahoo_app_id = false
+SitemapGenerator::Sitemap.create(
+    :include_root => true, :include_index => true,
+    :filename => :new_sitemaps, :sitemaps_path => 'fr/') do
 
-SitemapGenerator::Sitemap.add_links do |sitemap|
-  sitemap.add contents_path, :priority => 0.7, :changefreq => 'daily'
+  add('/one', :priority => 0.7, :changefreq => 'daily')
 
-  # add all individual articles
-  Content.find(:all).each do |c|
-    sitemap.add content_path(c), :lastmod => c.updated_at
+  # Test a new location and filename and sitemaps host
+  group(:sitemaps_path => 'en/', :filename => :xxx,
+      :sitemaps_host => "http://newhost.com") do
+
+    add '/two'
+    add '/three'
   end
 
-  sitemap.add "/merchant_path", :host => "https://www.example.com"
+  # Test a namer
+  group(:sitemaps_namer => SitemapGenerator::SitemapNamer.new(:abc, :start => 3)) do
+    add '/four'
+    add '/five'
+    add '/six'
+  end
+
+  add '/seven'
+
+  # This should be in a file of its own
+  group(:sitemaps_host => "http://exceptional.com") do
+    add '/eight'
+    add '/nine'
+  end
+
+  add '/ten'
+
+  # This should have no effect.  Already added default links.
+  group(:include_root => true, :include_index => true) {}
+
+  add "/merchant_path", :host => "https://www.merchanthost.com"
 end
