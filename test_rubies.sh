e3688cbbd2331a9753a3a53cab1a957679bd949e
Karl Varga <kjvarga@gmail.com>
2011-12-23 13:48:32 -0800

    Adjust the test rubies

--- test_rubies.sh
+++ test_rubies.sh
@@ -2,5 +2,5 @@
 
 # Run the specs across different rubies.
 set -o verbose
-RBXOPT="-Xrbc.db" rvm ree,1.8.7,1.9.1,1.9.2,1.9.3,jruby-1.6.5,jruby-1.6.4 exec bundle
-RBXOPT="-Xrbc.db" rvm ree,1.8.7,1.9.1,1.9.2,1.9.3,jruby-1.6.5,jruby-1.6.4 exec bundle exec rake spec
+RBXOPT="-Xrbc.db" rvm ree,1.9.1,1.9.2,1.9.3,jruby-1.6.5,jruby-1.6.4 exec bundle
+RBXOPT="-Xrbc.db" rvm ree,1.9.1,1.9.2,1.9.3,jruby-1.6.5,jruby-1.6.4 exec bundle exec rake spec
