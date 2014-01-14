9e378cac09f107fe1086481a92a33ce72774d834
Laurynas Butkus <laurynas.butkus@gmail.com>
2014-01-14 13:18:12 +0200

    Add FogAdapter

--- lib/sitemap_generator.rb
+++ lib/sitemap_generator.rb
@@ -12,6 +12,7 @@ module SitemapGenerator
   autoload(:FileAdapter, 'sitemap_generator/adapters/file_adapter')
   autoload(:S3Adapter,   'sitemap_generator/adapters/s3_adapter')
   autoload(:WaveAdapter, 'sitemap_generator/adapters/wave_adapter')
+  autoload(:FogAdapter,  'sitemap_generator/adapters/fog_adapter')
   autoload(:BigDecimal,  'sitemap_generator/core_ext/big_decimal')
   autoload(:Numeric,     'sitemap_generator/core_ext/numeric')
 
