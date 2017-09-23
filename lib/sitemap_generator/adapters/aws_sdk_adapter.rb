1a6899635da0ac483b96764f70b43e7b3eab5da1
Karl Varga <kjvarga@gmail.com>
2017-09-23 16:46:58 -0700

    Simplify AwsSdkAdapter Do not attempt to require the SDK.  Instead, detect the classes that we use and have the user do their own require.

--- lib/sitemap_generator/adapters/aws_sdk_adapter.rb
+++ lib/sitemap_generator/adapters/aws_sdk_adapter.rb
@@ -1,70 +1,56 @@
-begin
-  require 'aws-sdk'
-rescue LoadError
-  raise LoadError.new("Missing required 'aws-sdk'.  Please 'gem install "\
-                      "aws-sdk' and require it in your application, or "\
-                      "add: gem 'aws-sdk' to your Gemfile.")
+if !defined?(Aws::S3::Resource) or !defined?(Aws::Credentials)
+  raise "Error: Aws::S3::Resource and/or Aws::Credentials are not defined.\n\n"\
+        "Please `require 'aws-sdk'` - or another library that defines these classes."
 end
 
 module SitemapGenerator
-  # Class for uploading the sitemaps to an S3 bucket using the plain AWS SDK gem
+  # Class for uploading sitemaps to an S3 bucket using the AWS SDK gem.
   class AwsSdkAdapter
-    # @param [String] bucket name of the S3 bucket
-    # @param [Hash]   opts   alternate means of configuration other than ENV
-    # @option opts  [String] :aws_access_key_id instead of ENV['AWS_ACCESS_KEY_ID']
-    # @option opts  [String] :aws_region instead of ENV['AWS_REGION']
-    # @option opts  [String] :aws_secret_access_key instead of ENV['AWS_SECRET_ACCESS_KEY']
-    # @option opts  [String] :path use this prefix on the object key instead of 'sitemaps/'
-    def initialize(bucket, opts = {})
+    # Specify your AWS bucket name, credentials, and/or region.  By default
+    # the AWS SDK will auto-detect your credentials and region, but you can use
+    # the following options to configure - or override - them manually:
+    #
+    # Options:
+    #   :aws_access_key_id [String] Your AWS access key id
+    #   :aws_secret_access_key [String] Your AWS secret access key
+    #   :aws_region [String] Your AWS region
+    #
+    # @param [String] bucket Name of the S3 bucket
+    # @param [Hash] options AWS credential overrides, see above
+    def initialize(bucket, options = {})
       @bucket = bucket
-
-      @aws_access_key_id = opts[:aws_access_key_id]
-      @aws_region = opts[:aws_region]
-      @aws_secret_access_key = opts[:aws_secret_access_key]
-
-      @path = opts[:path] || 'sitemaps/'
+      @aws_access_key_id = options[:aws_access_key_id]
+      @aws_secret_access_key = options[:aws_secret_access_key]
+      @aws_region = options[:aws_region]
     end
 
     # Call with a SitemapLocation and string data
     def write(location, raw_data)
       SitemapGenerator::FileAdapter.new.write(location, raw_data)
-
-      s3_object_key = "#{@path}#{location.path_in_public}"
-      s3_object = s3_resource.bucket(@bucket).object(s3_object_key)
-
-      content_type = location[:compress] ? 'application/x-gzip' : 'application/xml'
+      s3_object = s3_resource.bucket(@bucket).object(location.path_in_public)
       s3_object.upload_file(location.path,
-                            acl: 'public-read',
-                            cache_control: 'private, max-age=0, no-cache',
-                            content_type: content_type)
+        acl: 'public-read',
+        cache_control: 'private, max-age=0, no-cache',
+        content_type: location[:compress] ? 'application/x-gzip' : 'application/xml'
+      )
     end
 
     private
 
     def s3_resource
-      @s3_resource ||= Aws::S3::Resource.new(s3_resource_option)
+      @s3_resource ||= Aws::S3::Resource.new(s3_resource_options)
     end
 
-    def s3_resource_option
-      option = {}
-      if has_specific_region?
-        option[:region] = @aws_region
-      end
-      if has_specific_credential?
-        option[:credentials] = Aws::Credentials.new(
+    def s3_resource_options
+      options = {}
+      options[:region] = @aws_region if !@aws_region.nil?
+      if !@aws_access_key_id.nil? && !@aws_secret_access_key.nil?
+        options[:credentials] = Aws::Credentials.new(
           @aws_access_key_id,
           @aws_secret_access_key
         )
       end
-      option
-    end
-
-    def has_specific_region?
-      !@aws_region.nil?
-    end
-
-    def has_specific_credential?
-      !@aws_access_key_id.nil? && !@aws_secret_access_key.nil?
+      options
     end
   end
 end
