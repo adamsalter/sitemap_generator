45a946f85066592fc36539d5861f319c847de6ee
Akira Matsuda <ronnie@dio.jp>
2018-02-02 20:54:33 +0900

    :warning: File.exists? is a deprecated name, use File.exist? instead

--- spec/support/file_macros.rb
+++ spec/support/file_macros.rb
@@ -10,20 +10,20 @@ module FileMacros
     end
 
     def file_should_exist(file)
-      expect(File.exists?(file)).to be(true), 'File #{file} should exist'
+      expect(File.exist?(file)).to be(true), 'File #{file} should exist'
     end
 
     def directory_should_exist(dir)
-      expect(File.exists?(dir)).to be(true), 'Directory #{dir} should exist'
+      expect(File.exist?(dir)).to be(true), 'Directory #{dir} should exist'
       expect(File.directory?(dir)).to be(true), '#{dir} should be a directory'
     end
 
     def directory_should_not_exist(dir)
-      expect(File.exists?(dir)).to be(false), 'Directory #{dir} should not exist'
+      expect(File.exist?(dir)).to be(false), 'Directory #{dir} should not exist'
     end
 
     def file_should_not_exist(file)
-      expect(File.exists?(file)).to be(false), 'File #{file} should not exist'
+      expect(File.exist?(file)).to be(false), 'File #{file} should not exist'
     end
 
     def identical_files?(first, second)
